e8a96fefc7b8f20ed962170f690ed748d97b0885
hzhq1255 <hzhq1255@163.com>
2022-07-28 17:23:24 +0800

    react demo complete

--- src/index.js
+++ src/index.js
@@ -37,6 +37,10 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    // judge winner and if table filed return
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     console.log(squares);
     squares[i] = this.state.xIsNext ? "X" : "O";
     this.setState({
@@ -55,7 +59,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player: X";
+    const winner = calculateWinner(this.state.squares);
+    let status = "";
+    if (winner) {
+      status = "The winner is " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
 
     return (
       <div>
@@ -94,6 +104,26 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
